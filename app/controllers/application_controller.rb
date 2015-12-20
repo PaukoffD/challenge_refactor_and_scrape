a42855b85e0aba3578ace6be3e9e11b151be2811
Dmitri Koulikoff <dima@koulikoff.ru>
2015-12-20 15:22:03 +0300

    Method ApplicationController#current_user is added as stub !

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # FIXME: here should be a real objec of User returned
+  def current_user
+    'current_user'
+  end
 end
