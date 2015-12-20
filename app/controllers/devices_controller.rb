e94dea0b4a49427759537eb4a5ada5853cd5c7f9
Dmitri Koulikoff <dima@koulikoff.ru>
2015-12-20 18:34:28 +0300

    Comments added, some variables renamed, parts of the code rearranged +

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -20,17 +20,24 @@ class DevicesController < InheritedResources::Base
     end
 
     if request.post?
-      data          = {}
-      updated_lines = {}
-      lookups       = {}
-      delete_lines  = []
+      lookups          = {}
+      data             = {}
+      updated_devices  = {}
+      removed_devices  = []
       clear_existing_data = params[:clear_existing_data]
 
+      # Here we form the lookups Hash that for each key that can appear
+      # in the input record comum name supplies the Hash of pairs vaule => :id
+      #
+      # If an association Class respond_to :export_key the name returned
+      # will be used insted of "name"
       Device.import_export_columns.each do |col|
         if col =~ /^(\w+)_id/
           case col
-          when 'device_make_id'  then lookups[col] = Hash[DeviceMake.pluck(:name, :id)]
-          when 'device_model_id' then lookups[col] = Hash[DeviceModel.pluck(:name, :id)]
+          when 'device_make_id'
+            lookups[col] = Hash[DeviceMake.pluck(:name, :id)]
+          when 'device_model_id'
+            lookups[col] = Hash[DeviceModel.pluck(:name, :id)]
           else
             Device.reflections.each do |k, reflection|
               if reflection.foreign_key == col
@@ -46,10 +53,10 @@ class DevicesController < InheritedResources::Base
             end
           end
         end
-      end
+      end   # Device.import_export_columns.each
 
-      @customer.accounting_types.each do |at|
-        lookups["accounting_categories[#{at.name}]"] = Hash[Hash[at.accounting_categories.pluck(:name, :id)].map{ |k,v| [k.strip, v] }]
+      @customer.accounting_types.each do |acc_type|
+        lookups["accounting_categories[#{acc_type.name}]"] = Hash[acc_type.accounting_categories.pluck(:name, :id).map{|k,v| [k.strip, v]}]
       end
 
       # We need this in a string since we parse it twice and Ruby will
@@ -61,6 +68,7 @@ class DevicesController < InheritedResources::Base
 
       contents = import_file.tempfile.read.encode(invalid: :replace, replace: '')
 
+      # Check that input file has only existing accounting_categories names
       begin
         CSV.parse(contents, headers: true, encoding: 'UTF-8').each do |row|
           row.headers.each do |header|
@@ -81,55 +89,57 @@ class DevicesController < InheritedResources::Base
       end
 
       begin
-        CSV.parse(contents, headers: true, encoding: 'UTF-8').each_with_index do |p, idx|
+        CSV.parse(contents, headers: true, encoding: 'UTF-8').each_with_index do |row, index|
           accounting_categories = []
-          hsh = p.to_hash.merge(customer_id: @customer.id)
+          row_data = row.to_hash.merge(customer_id: @customer.id)
 
-          if '' == p['number'].to_s.strip
-            (@errors['General'] ||= []) << "An entry around line #{idx} has no number"
-            next
-          end
-
+          # Check validity of the number
           # Hardcode the number, just to make sure we don't run into issues
-          hsh['number'] = p['number'] = hsh['number'].gsub(/\D+/,'')
+          number = row_data['number'] = row_data['number'].gsub(/\D+/,'')
+          (@errors['General'] ||= []) << "An entry around line #{index} has no number" and next if number.blank?
+          (@errors[number] ||= []) << "Is a duplicate entry" and next if data[number]
 
-          if data[p['number']]
-            (@errors[p['number']] ||= []) << "Is a duplicate entry"
-            next
-          end
+          # Process sthrin assingments
+          row_data = Hash[row_data.map{|k, v| [k, v =~ /^="(.*?)"/ ? $1 : v] }]
 
-          hsh = Hash[hsh.map{ |k,v| [k, v =~ /^="(.*?)"/ ? $1 : v] }]
-
-          hsh.dup.each do |k,v|
-            if lookups.key?(k)
-              if k =~ /accounting_categories/
-                accounting_category_name = k.gsub(/accounting_categories\[(.*?)\]/, '\1')
-                val = lookups[k][v.to_s.strip]
+          # Replace the values with the ids from lookups
+          row_data.keys.each do |attr|
+            value = row_data[attr]
+            if lookups.key? attr
+              if attr =~ /accounting_categories/
+                accounting_type = attr.gsub(/accounting_categories\[(.*?)\]/, '\1')
+                val = lookups[attr][value.to_s.strip]
                 if !val
-                  (@errors[p['number']] ||= []) << "New \"#{accounting_category_name}\" code: \"#{v.to_s.strip}\""
+                  # Why not to create a new accounting_type?
+                  (@errors[number] ||= []) << "New \"#{accounting_type}\" code: \"#{value.to_s.strip}\""
                 else
-                  accounting_categories << lookups[k][v.to_s.strip]
+                  accounting_categories << val
                 end
-                hsh.delete(k)
+                row_data.delete(attr)
               else
-                hsh[k] = lookups[k][v]
+                row_data[attr] = lookups[attr][value]
               end
-            end
-            if v == 't' || v == 'f'
+            end   # if lookups.key? attr
+            # Boolean values
+            if value == 't' || value == 'f'
               # This is postgres-specific
-              hsh[k] = (v == 't')
+              row_data[attr] = (value == 't')
             end
           end
-          hsh['accounting_category_ids'] = accounting_categories unless accounting_categories.empty?
-          data[p['number']] = hsh.select{ |k,v| k }
+          row_data['accounting_category_ids'] = accounting_categories if accounting_categories.present?
+          # Are there any?
+          data[number] = row_data.select{|attr, value| attr }
         end
       rescue => e
         @errors['General'] = [e.message]
       end
 
-      duplicate_numbers = Device.where(number: data.keys).where.not(customer_id: @customer.id)
-      duplicate_numbers.each do |device|
-        if !device.cancelled? && data[device.number]['status'] != 'cancelled'
+      # Duplicated_numbers
+      Device.where(number: data.keys)
+        .where.not(customer_id: @customer.id)
+        .where.not(status: 'cancelled')
+      .each do |device|
+        if data[device.number]['status'] != 'cancelled'
           (@errors[device.number] ||= []) << "Duplicate number. The number can only be processed once, please ensure it's on the active account number."
         end
       end
@@ -137,61 +147,51 @@ class DevicesController < InheritedResources::Base
       # Shortcut here to get basic errors out of the way
       return if @errors.length > 0
 
-      lines = @customer.devices.to_a
-      lines.each do |line|
-        if data.has_key?(line.number)
-          updated_lines[line.number] = line
+      @customer.devices.each do |device|
+        if data.has_key?(device.number)
+          updated_devices[device.number] = device
         elsif clear_existing_data
-          delete_lines << line
+          removed_devices << device
+        end
+      end
+
+      data.each do |number, attributes|
+        updated_devices[number] = Device.new unless updated_devices[number]
+      end
+
+      number_conditions = []
+      updated_devices.each do |number, device|
+        device.assign_attributes(data[number])
+        @errors[number] = device.errors.full_messages unless device.valid?
+        number_conditions << "(number = '#{number}' AND status <> 'cancelled')" unless device.cancelled?
+      end
+
+      if number_conditions.size.nonzero?
+        invalid_devices = Device.unscoped.where("(#{number_conditions.join(' OR ')}) AND customer_id != ?", @customer.id)
+        invalid_devices.each do |device|
+          (@errors[device.number] ||= []) << "Already in the system as an active number"
         end
       end
 
       Device.transaction do
-        updated_lines.each do |number, line|
-          line.assign_attributes(data[number])
-        end
-
-        data.each do |number, attributes|
-          unless updated_lines[number]
-            updated_lines[number] = Device.new
-            updated_lines[number].assign_attributes(attributes)
-          end
-        end
-
-        updated_lines.each do |number, line|
-          unless line.valid?
-            @errors[number] = line.errors.full_messages
-          end
-        end
-
-        number_conditions = []
-        updated_lines.each do |number, device|
-          number_conditions << "(number = '#{number}' AND status <> 'cancelled')" unless device.cancelled?
-        end
-
-        if number_conditions.size.nonzero?
-          invalid_devices = Device.unscoped.where("(#{number_conditions.join(' OR ')}) AND customer_id != ?", @customer.id)
-          invalid_devices.each do |device|
-            (@errors[device.number] ||= []) << "Already in the system as an active number"
-          end
-        end
-
         if @errors.empty?
-          updated_lines.each do |number, line|
-            line.track!(created_by: current_user, source: "Bulk Import") { line.save(validate: false) }
+          updated_devices.each do |number, device|
+            device.track!(created_by: current_user, source: "Bulk Import") do
+              device.save(validate: false)
+            end
           end
 
           if params[:clear_existing_data]
-            delete_lines.each{ |line| line.delete }
+            removed_devices.each{ |device| device.delete }
           end
 
-          flash[:notice] = "Import successfully completed. #{updated_lines.length} lines updated/added. #{delete_lines.length} lines removed."
+          flash[:notice] = "Import successfully completed. #{updated_devices.length} lines updated/added. #{removed_devices.length} lines removed."
         else
           raise ActiveRecord::Rollback
         end
-      end
-    end
-  end
+      end   # Device.transaction
+    end   # if request.post?
+  end   # import
 
   private
 
