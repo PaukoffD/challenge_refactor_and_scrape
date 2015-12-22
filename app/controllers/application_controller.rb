a585d36b203279191b43bf6c7f8408fa6e01089e
Dmitri Koulikoff <dima@koulikoff.ru>
2015-12-23 02:57:17 +0300

    Typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # FIXME: here should be a real objec of User returned
+  # FIXME: here should be a real object of User returned
   def current_user
     'current_user'
   end
