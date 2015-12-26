4786e4270fb51ecda2e3af8c506d3379c6c4ac4f
Dmitri Koulikoff <dima@koulikoff.ru>
2015-12-26 17:48:04 +0300

    Navigations links are added !

--- spec/views/delivery_companies/index.html.haml_spec.rb
+++ spec/views/delivery_companies/index.html.haml_spec.rb
@@ -11,13 +11,13 @@ describe "delivery_companies/index", type: :view do
 
     assert_select 'tr>td', text: @delivery_company.name.to_s, count: 1
     assert_select 'tr>td', text: @delivery_company.url.to_s, count: 1
-    assert_select 'tr>td', text: @delivery_company.form_name.to_s, count: 1
-    assert_select 'tr>td', text: @delivery_company.form_action.to_s, count: 3
-    assert_select 'tr>td', text: @delivery_company.field_name.to_s, count: 1
-    assert_select 'tr>td', text: @delivery_company.extra_fields.to_s, count: 3
-    assert_select 'tr>td', text: @delivery_company.extra_values.to_s, count: 3
-    assert_select 'tr>td', text: @delivery_company.submit.to_s, count: 1
-    assert_select 'tr>td', text: @delivery_company.xpath.to_s, count: 1
+    assert_select 'tr>td', text: @delivery_company.form_name.to_s, count: 0
+    assert_select 'tr>td', text: @delivery_company.form_action.to_s, count: 0
+    assert_select 'tr>td', text: @delivery_company.field_name.to_s, count: 0
+    assert_select 'tr>td', text: @delivery_company.extra_fields.to_s, count: 0
+    assert_select 'tr>td', text: @delivery_company.extra_values.to_s, count: 0
+    assert_select 'tr>td', text: @delivery_company.submit.to_s, count: 0
+    assert_select 'tr>td', text: @delivery_company.xpath.to_s, count: 0
   end
 
 end
